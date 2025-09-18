9f04173786eec59d98a3a97f095913f36b2ddcd1
PietroBelafontePansani <pietrobelafontepansani@gmail.com>
2025-09-18 17:54:06 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,46 @@
+# sinais
+#aritmérticos
+
+# +  -   *   /
+
+# módulo%   divisão com 2 barras//     raiz **(0.5)
+
+print(251000%411)
+print(10//5)
+print(81**(0.5))
+
+# PARENTESE EXPONENCIAÇÃO MULTIPLICAÇÃO DIVISÃO ADIÇÃO SUBTRAÇÃO
+
+print(10 +  2  + 3)
+
+print(10 > 2)
+print(10 < 2)
+print(10 >= 2)
+print(10 <= 2)
+print(10 == 2)
+print(10 != 2)
+
+# CONSTANTES 
+
+TESTE = 10
+print(TESTE)
+
+TESTE = 100
+print(TESTE)
+
+# NÃO ALTERE
+
+def teste():
+    return  100
+
+
+TESTE  =  teste()
+
+ 
+
+
+
+
+
+
+
