c83f41f4975d7f7e689d3e5564b33af687523323
PietroBelafontePansani <pietrobelafontepansani@gmail.com>
2025-09-24 16:42:11 -0300

    Update aula4.py

--- main.py
+++ main.py
@@ -1,1 +1,189 @@
+# Questões da aula 6
+multi = (4 * 6)/2
+print('resultado', multi)
+
+soma = 5 + 7
+print('resultado', soma)
+
+potência = 3 ** 4
+print('resultado', potência)
+
+media = (8 + 12 + 15)/2
+print('resultado', media)
+
+subtraia = (10 - 2)*5
+print('resultado', subtraia)
+
+divida = (27 / 3)+5
+print('resultado', divida)
+
+mod = 17 % 4 
+print('resultado', mod)
+
+multi = (9 * 3) ** 2
+print('resultado', multi)
+
+raiz = 81 ** 0.5
+print('resultado', raiz)
+
+resultado = (20 + 3) * 4
+print(resultado)
+
+multi = 15 * 2 -7
+print('resultado', multi)
+
+resultado = 5 ** 3
+print(resultado)
+
+media = (17 + 21 + 25)/2
+print('resultado', media)
+
+multi = (11 * 2)*7
+print('resultado', multi)
+
+resultado = ((3 * 8 )-15)/2
+print(resultado)
+
+resultado = 2 ** 10
+print(resultado)
+# Utilize as 5 concatenações, teste todas 
+
+# 1 Imprima uma mensagem de boas-vindas na tela.
+print('Olá bom dia!')
+
+
+
+# 2 Declare uma variável booleana verdadeiro
+# com o valor True e imprima seu tipo
+
+boleano = True
+print(type(boleano))
+
+
+# 3 Imprima o resultado da multiplicação de dois 
+# números decimais de sua escolha
+
+n1 = float(input('numero1:'))
+n2 = float(input('numero2:'))
+
+print(n1 * n2)
+
+# 4 Imprima o resultado da divisão (/)de dois números 
+# inteiros de sua escolha.
+
+n1 = int(input('numero1:'))
+n2 = int(input('numero2:'))
+
+print(n1 / n2)
+
+# 5 Imprima o resultado da subtração de dois números 
+# inteiros de sua escolha
+
+n1 = int(input('numero1:'))
+n2 = int(input('numero2:'))
+
+print(n1 - n2)
+
+
+
+# 6 Imprima o resultado da divisão (//)inteira de dois números inteiros 
+# de sua escolha.
+
+
+n1 = int(input('numero1:'))
+n2 = int(input('numero2:'))
+
+print(n1 // n2)
+
+# 7 Imprima o resultado da multiplicação de 4 números 
+# decimais de sua escolha
+
+print(0.10 * 20.5 * 5.5  * 10.0)
+
+
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, 
+# imprima o dobro desse número
+
+numero = 10
+dobro =  numero ** 2
+print(dobro)
+
+# 9 Crie um sistema de cadastro com as estruturas 
+# que vc já 
+# conhece(Use apenas input, print e variavel)
+
+
+nome = input('NOme: ')
+idade = int(input('Idade: '))
+endereco = input('Endereço:')
+curso =  input('Curso: ')
+
+print(f'''Dados:' 
+      
+      nome - {nome}
+      idade - {idade}
+      endereço - {endereco}
+      curso - {curso} 
+
+
+
+''')
+
+# concatenação -  juntar dados - literal variavel 
+
+
+nome  =  input('Nome: ')
+idade = int(input('Idade: '))
+
+
+print('Ola',nome, 'sua idade é', idade)
+print('Olá' + ' ' +nome)
+print(f'Olá {nome}')
+print('Olá {}'.format(nome))
+print('Olá %s'%(nome))
+# concatenação -  juntar dados - literal variavel 
+
+
+nome  =  input('Nome: ')
+idade = int(input('Idade: '))
+
+
+print('Ola',nome, 'sua idade é', idade)
+print('Olá' + ' ' +nome)
+print(f'Olá {nome}')
+print('Olá {}'.format(nome))
+print('Olá %s'%(nome))
+
+
+
+
+# Identação  - organizar o código
+
+
 print('teste')
+print('teste')
+
+
+
+# Refatoração identificar erros de bugs 
+
+
+# Sintaxe
+# Semantica *** 
+
+# Questões da aula 6
+if 10 > 2:
+    print('10 é maior')
+else:
+    print('10 é maior')   
+
+    multi = (4 * 6)/2
+    print('resultado', mutli)
+
+    soma = 5 + 7
+    print('resultado', soma)
+
+    potência = 3 ** 4
+    print('resultado', potência)
+
+    
