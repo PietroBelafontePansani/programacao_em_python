4039504e36ad21ed617cf90c3bbd4e556033736d
PietroBelafontePansani <pietrobelafontepansani@gmail.com>
2025-09-25 17:56:39 -0300

    Update listaspython.py

--- listaspython.py
+++ listaspython.py
@@ -42,3 +42,73 @@ print(carros, numeros)
 
 carros.extend(numeros)
 print(carros)
+
+dicionario = {
+'chave':'valor',
+'chave':'valor',
+}
+
+produtos = ['livros','tablets','fones']
+
+produtos = {'livros':{30.00},
+            'tablets':{900.00},
+            'fones':{65.00},
+
+            'carrinho':{'livros','tablets','fones'},
+            'forma_de_pagamento':{'cd','cc','pix'}
+            
+
+            
+            
+            
+            }
+print(produtos)
+
+# Crie um e-commerce com a estrutura de dicionários.
+
+
+# Produtos:  Livros, tablets e fones de ouvido
+
+
+e_commerce ={
+
+
+'Livros':{
+   'a':20.0,
+   'b':30.0,
+   'c':40},
+'tables':{
+'samsumng':100.0,
+'motorola':1000.0,
+},
+'fones':{
+'a':50,
+'b':60
+}   
+}
+
+
+
+e_commerce2={
+
+
+'table':20,
+'fone':10,
+'livro':100
+
+
+
+}
+
+
+
+e_commerce3 = {
+
+
+'fone':['a','b','c'],
+'valore_fones':[10,20,30],
+'tablets':['a','b','c'],
+'livro':['a','b','c'],
+'cpf':(123,456,78),
+'teste':{10,20,30}
+}
