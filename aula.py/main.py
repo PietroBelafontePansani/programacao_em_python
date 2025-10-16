d2df9139790b39331e977cbee4abbbcad3ad3b41
PietroBelafontePansani <pietrobelafontepansani@gmail.com>
2025-10-16 15:58:15 -0300

    Add files via upload

---
+++ aula.py/main.py
@@ -0,0 +1,20 @@
+print(' Scrum Master é um líder servidor que ajuda a equipe a seguir os princípios e práticas do Scrum')
+print('_________________________________________________________________________________________________')
+print('importância garantir que a equipe siga os princípios e práticas do Scrum, agindo como um líder servidor que remove impedimentos,' \
+' facilita a comunicação e a colaboração e promove a melhoria contínua do processo')
+print('____________________________________________________________________________________________________________________________________')
+print('"PO" pode significar tanto Product Owner quanto Purchase Order (ordem de compra), dependendo do contexto')
+print('__________________________________________________________________________________________________________')
+print('importância pode se referir a Product Owner, um papel-chave em metodologias ágeis para maximizar o valor do produto; à Pesquisa Operacional,' \
+' uma ferramenta para otimizar decisões e recursos em cenários complexos; ou a Procedimentos Operacionais, que padronizam rotinas para aumentar produtividade e qualidade')
+print('_________________________________________________________________________________________________________________________________________________________________________')
+print('líder de equipe de TI é um profissional que combina expertise técnica com habilidades de gestão para guiar uma equipe na entrega de projetos e soluções tecnológicas')
+print('______________________________________________________________________________________________________________________________________________________________________')
+print('importância ele alinha a tecnologia aos objetivos de negócio, gerencia equipes, otimiza infraestruturas, garante a segurança da informação e impulsiona a inovação')
+print('____________________________________________________________________________________________________________________________________________________________________')
+print('equipe de tecnologia é um conjunto de profissionais responsáveis por gerenciar a infraestrutura e as soluções tecnológicas de uma organização, o que inclui o suporte técnico,' \
+' o desenvolvimento de software e a segurança de dados')
+print('________________________________________________________________________________________________________________________________________________________________________________')
+print('importância garantir que os sistemas de TI estejam funcionando perfeitamente, desenvolver e implementar soluções personalizadas, proteger dados e dar suporte aos usuários,' \
+' além de agir como parceira estratégica para alinhar a tecnologia aos objetivos de negócio')
+print('____________________________________________________________________________________________________________________________________________________________________________________')
